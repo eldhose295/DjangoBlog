d082046f6b072cdad54be6d8fce2223fdde3851e
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-10-31 16:55:48 +0530

    Created about page route

--- user/views.py
+++ user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome")
+
+def about(request):
+    return HttpResponse("About page")
